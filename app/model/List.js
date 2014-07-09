f7998d9c5da4ef9c204e238538ddabe0daee70ae
中村久司 <hisashi.nakamura@xenophy.com>
2014-07-09 14:25:55 +0900

    データ更新の実装

--- app/model/List.js
+++ app/model/List.js
@@ -3,6 +3,23 @@ Ext.define('MyList.model.List', {
 
     fields: [
         {name: 'group', type: 'number'}
-    ]
+    ],
+
+    proxy: {
+        type: 'direct',
+        api: {
+            read: 'MyList.rpc.MyList.getList',
+            create: 'MyList.rpc.MyList.createData',
+            update: 'MyList.rpc.MyList.updateData',
+            destroy: 'MyList.rpc.MyList.removeData'
+        },
+        reader: {
+            type: 'json',
+            rootProperty: 'data'
+        },
+        writer: {
+            type: 'json'
+        }
+    }
 
 });
